271e34973067790ff56914e42e219208954ec748
Chris Marsh <chrismarsh.c2@gmail.com>
2021-03-12 17:05:19 -0600

    stream line build.py

--- build.py
+++ build.py
@@ -1,26 +1,36 @@
 from cpt.packager import ConanMultiPackager
 from collections import defaultdict
+import os
 
 if __name__ == "__main__":
     builder = ConanMultiPackager(cppstds=[14],
                                 archs=["x86_64"],
                                 build_types=["Release"])
+
+    libcxx = 'libstdc++11' #gcc
+    if 'clang' in os.environ['COMPILER']:
+        libcxx='libc++'
+        
+
+
+    builder.add(settings={"compiler.libcxx":libcxx},
+                options={}, env_vars={}, build_requires={})
                               
-    builder.add_common_builds(pure_c=False,shared_option_name="proj:shared")
+    # builder.add_common_builds(pure_c=False,shared_option_name="proj:shared")
 
-    builder.remove_build_if(lambda build: build.settings["compiler.libcxx"] == "libstdc++")
+    # builder.remove_build_if(lambda build: build.settings["compiler.libcxx"] == "libstdc++")
 
-    named_builds = defaultdict(list)
-    for settings, options, env_vars, build_requires, reference in builder.items:
+    # named_builds = defaultdict(list)
+    # for settings, options, env_vars, build_requires, reference in builder.items:
 
-        shared="shared"
+    #     shared="shared"
 
-        if not options['proj:shared']:
-            shared = "static" 
+    #     if not options['proj:shared']:
+    #         shared = "static" 
 
-        named_builds[settings['compiler'] +"_"+shared].append([settings, options, env_vars, build_requires, reference])
+    #     named_builds[settings['compiler'] +"_"+shared].append([settings, options, env_vars, build_requires, reference])
 
-    builder.named_builds = named_builds
+    # builder.named_builds = named_builds
 
     builder.run()
 
